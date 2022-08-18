94ef8c87681c8f45288fa910b2d4b73bfc74399d
DTGuy <tguy5837@gmail.com>
2022-08-18 16:41:09 -0400

    got start info to hide on start button click

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,15 +1,40 @@
 var startBtn = document.querySelector("#start-btn");
 var timer = document.querySelector("#timer");
 
+var questionArray = [
+    {
+        question: "I am a question.",
+        choices: ["Answer1", "Answer2", "Answer3", "Answer4"],
+        answer: "Answer2"
+    }
+]
 
 var startTimer = function () {
+    // Display start time
     var timeRemaining = 75;
-    if (timeRemaining > 0) {
-        setInterval(function () {
+    timer.textContent = ("Time Remaining: " + timeRemaining)
+    // Start counting down
+    var countDown = setInterval(function () {
+        if (timeRemaining > 0) {
             timeRemaining = timeRemaining - 1;
             timer.textContent = ("Time Remaining: " + timeRemaining)
-        }, 1000)
-    }
+        }
+        // When timer runs out:
+        else if (timeRemaining <= 0) {
+            clearInterval(countDown);
+        }
+    }, 1000)
+
 };
 
+var startQuiz = function () {
+    var showEl = document.querySelector("#startInfo")
+    showEl.setAttribute("class", "hide");
+
+    var questions = function () {
+
+    }
+}
+
 startBtn.addEventListener("click", startTimer);
+startBtn.addEventListener("click", startQuiz);
